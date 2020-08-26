f43b3784e398208421a65681431fc68a3656026c
Ilya Belov <underbottom1337@gmail.com>
2020-08-26 22:30:06 +0300

    add todo item

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from "react";
-import "./App.css";
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import './App.scss';
+import { TodoItemForm } from './Components/TodoItemForm';
+import { Container } from '@material-ui/core';
+
+export interface TodoItem {
+  id: number;
+  text: string;
+}
+
+export interface TodoItemState {
+  items: TodoItem[];
+  setItems: Dispatch<SetStateAction<TodoItem[]>>;
+}
 
 function App() {
-  return <div></div>;
+  const [items, setItems] = useState<TodoItem[]>([]);
+
+  return (
+    <Container maxWidth="lg">
+      <TodoItemForm setItems={setItems} items={items} />
+    </Container>
+  );
 }
 
 export default App;
--- src/Components/TodoItemForm/index.tsx
+++ src/Components/TodoItemForm/index.tsx
@@ -1,16 +1,44 @@
 import { TextField, Button, Icon } from '@material-ui/core';
-import React from 'react';
+import React, { Dispatch, SetStateAction, useState } from 'react';
 import './index.scss';
+import { TodoItem, TodoItemState } from '../../App';
 
-export const TodoItemForm: React.FC = () => {
+export interface TodoItemFormData {
+  items: TodoItem[];
+  setItems: Dispatch<SetStateAction<TodoItem[]>>;
+}
+
+export interface LastItemIdState {
+  lastItemId: number;
+  setLastItemId: Dispatch<SetStateAction<number>>;
+}
+
+export const TodoItemForm: React.FC<TodoItemState> = ({ items, setItems }) => {
+  const [inputValue, setInputValue] = useState('');
+  const [lastItemId, setLastItemId] = useState<number>(0);
+
+  function addNewTodoItem() {
+    setItems([...items, { id: lastItemId + 1, text: inputValue }]);
+    setInputValue('');
+    setLastItemId(lastItemId + 1);
+  }
+
   return (
     <form className={'todo-item-form'} noValidate autoComplete="off">
-      <TextField id="todo-item-input" label="Todo" variant="standard" className={'add-input'} />
+      <TextField
+        id="todo-item-input"
+        value={inputValue}
+        onChange={($event) => setInputValue($event.target.value)}
+        label="Todo"
+        variant="standard"
+        className={'add-input'}
+      />
       <Button
         variant="contained"
         className={'add-button'}
         color="primary"
         endIcon={<Icon>add</Icon>}
+        onClick={() => addNewTodoItem()}
       >
         Add
       </Button>
