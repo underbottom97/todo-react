3ed0a87d724af30d26a0bbee179cab94edda34bf
Ilya Belov <underbottom1337@gmail.com>
2020-08-27 00:37:17 +0300

    some fixes

--- src/Components/TodoItemList/index.tsx
+++ src/Components/TodoItemList/index.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TodoItemState } from '../../App';
-import { List } from '@material-ui/core';
+import { Checkbox, FormControlLabel, List } from '@material-ui/core';
 import { TodoItem } from '../TodoItem';
+import { TodoItemStatusType } from '../../enums/TodoItemStatusType';
 
 export const TodoItemList: React.FC<TodoItemState> = ({ items, setItems }) => {
+  const [showDeletedItems, setShowDeletedItems] = useState(false);
+
   return (
     <List>
-      {items.map((item) => {
-        return <TodoItem key={item.id} item={item} setItems={setItems} items={items} />;
-      })}
+      <FormControlLabel
+        control={
+          <Checkbox
+            id={'123421'}
+            checked={showDeletedItems}
+            onChange={() => setShowDeletedItems(!showDeletedItems)}
+            name="showDeleted"
+          />
+        }
+        label="Show deleted"
+      />
+
+      {items
+        .filter((item) => {
+          if (showDeletedItems) {
+            return true;
+          }
+          return item.status !== TodoItemStatusType.deleted;
+        })
+        .map((item) => {
+          return <TodoItem key={item.id} item={item} setItems={setItems} items={items} />;
+        })}
     </List>
   );
 };
